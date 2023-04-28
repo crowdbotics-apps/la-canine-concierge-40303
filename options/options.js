ecaf3508a2bb640d687ca06fe1fdbd6494608a6a
Crowdbotics <team@crowdbotics.com>
2023-04-28 04:10:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "la-canine-concierge-40303",
+  url: "https://la-canine-concierge-40303.botics.co",
+  api: "https://la-canine-concierge-40303.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
